c5be963bc891a4de2c7d55d93979a9d8c41a3f98
HenryDrewSchool2 <henrybishop@drewschool.org>
2025-05-15 09:46:26 -0700

    Create Fire.java

---
+++ Fire.java
@@ -0,0 +1,18 @@
+import org.code.theater.*;
+import org.code.media.*;
+
+class Fire extends Randomon {
+
+public String weakAttack = "Ember";
+public String strongAttack = "Pyroblast";
+public static int fireDamageWeak = 100;
+public static int fireDamageStrong = 250;
+  
+
+//public void randomonAttack(String attackType) {
+  //this.attackType = attackType;
+ // this.damage = damage;
+//}
+
+  
+}
