cb893e60755355669e71c20ebdb173f306e0289b
HenryDrewSchool2 <henrybishop@drewschool.org>
2025-05-15 09:46:52 -0700

    Create Water.java

---
+++ Water.java
@@ -0,0 +1,16 @@
+import org.code.theater.*;
+import org.code.media.*;
+
+class Water extends Randomon {
+
+public String weakAttack = "Water Gun";
+public String strongAttack = "Hydro Cannon";
+public static int waterDamageWeak = 100;
+public static int waterDamageStrong = 250;
+
+//public void randomonAttack(String attackType) {
+//  this.attackType = attackType;
+ // this.damage = damage;
+//}
+  
+}
