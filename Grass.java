6708cb2ce22b1ddd4f314f0e07cd7d130a00e83d
HenryDrewSchool2 <henrybishop@drewschool.org>
2025-05-15 09:47:09 -0700

    Create Grass.java

---
+++ Grass.java
@@ -0,0 +1,19 @@
+import org.code.theater.*;
+import org.code.media.*;
+
+class Grass extends Randomon {
+
+public String weakAttack = "Razor Leaf";
+public String strongAttack = "Plant Frenzy";
+public static int grassDamageWeak = 100;
+public static int grassDamageStrong = 250;
+//public void randomonAttack(String attackType) {
+ // this.attackType = attackType;
+ // this.damage = damage;
+
+
+//if (attackType.equals(weakAttack)) {
+ // damage = 20;
+//}
+//}
+}
